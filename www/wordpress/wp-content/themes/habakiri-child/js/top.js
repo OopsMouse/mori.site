d5dce56260ece7baa4227d44ff505dc3425ad04d
ichikawa shunta <shunta27ichikawa@gmail.com>
2015-08-16 19:55:15 +0900

    グローバルメニュー、一旦完成 トップページ戻るボタン、jsにて実装

--- www/wordpress/wp-content/themes/habakiri-child/js/top.js
+++ www/wordpress/wp-content/themes/habakiri-child/js/top.js
@@ -1,5 +1,5 @@
+//画面サイズに合わせてheightを指定,CSS書き換えます
 jQuery(function($){
-  //画面サイズに合わせてheightを指定,CSS書き換えます
   $(document).ready(function () {
     hsize = $(window).height();
     $("section").css("height", hsize + "px");
@@ -8,20 +8,32 @@ jQuery(function($){
     hsize = $(window).height();
     $("section").css("height", hsize + "px");
   });
+
   // #で始まるアンカーをクリックした場合の処理
   jQuery('a[href^=#]').click(function() {
-    var headerHight = 0;
     var speed = 400; // ミリ秒
     var href= jQuery(this).attr("href");
     var target = jQuery(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top-headerHight;
+    var position = target.offset().top;
     jQuery('body,html').animate({scrollTop:position}, speed, 'swing');
     return false;
   });
+});
 
-
-
-
-
-
+//ページトップへ移動するボタン
+jQuery(function(){
+  jQuery(window).scroll(function(){
+    var now = jQuery(window).scrollTop();
+    if(now > 300){
+      jQuery('#page-top').fadeIn('slow');
+    }else{
+      jQuery('#page-top').fadeOut('slow');
+    }
+  });
+  jQuery('#move-page-top').click(function(){
+  //ページトップへ移動する
+  jQuery('body,html').animate({
+          scrollTop: 0
+      }, 400);
+  });
 });
